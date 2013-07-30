9ae453812b1edd786ac89162fe5e13dd794d65d3
Sven-S. Porst <porst@sub.uni-goettingen.de>
2013-07-30 09:55:49 +0200

    move Projects folder top level

---
+++ jfk-institut-zeitungen.ts
@@ -0,0 +1,48 @@
+plugin.tx_find {
+	settings {
+		connection {
+			host = 127.0.0.1
+			port = 80
+			path = /solr-jfk/
+		}
+		queryFields {
+		}
+		additionalFilters {
+		}
+		sort {
+			1 {
+				id = default
+				sortCriteria = title_sort asc
+			}
+		}
+		standardFields {
+			title = title
+			snippet = publisher
+		}
+		facets {
+			10 {
+				id = buchstabe
+				field = title_facet
+				sortOrder = index
+				type = Tabs
+				excludeOwnFilter = 1
+				hidden = 1
+				selectedByDefault {
+					0 = 1
+				}
+			}
+		}
+		highlight {
+			fields.1 = *
+		}
+
+		CSSPaths.50 = EXT:find/Projects/jfk-institut-zeitungen/Resources/jfk.css
+		JSPaths.50 = EXT:find/Projects/jfk-institut-zeitungen/Resources/jfk.js
+
+		languageRootPath = EXT:find/Projects/jfk-institut-zeitungen/Language/
+	}
+	view {
+		templateRootPath = EXT:find/Projects/jfk-institut-zeitungen/Templates/
+		# partialRootPath = EXT:find/Projects/jfk-institut-zeitungen/Partials/
+	}
+}
